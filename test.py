58028b631a007c2411c3f02fa8ab22e263769972
Ngoc Nguyen <minhngocmn45@gmail.com>
2024-08-06 13:07:36 +0200

    create Google matrix with example

--- test.py
+++ test.py
@@ -2,16 +2,22 @@ import pandas as pd
 import numpy as np
 from pandas.core.interchange import column
 
+#define a scalar between 0 and 1
+alpha = 0.9
+
 #Read Excel Sheet
-df = pd.read_excel('Mappe1.xlsx', header=0, names=["Parent", "Child"])
+df = pd.read_excel('test2.xlsx', header=0, names=["Parent", "Child"])
 
 #count amount of links
 count_rows = df[df.columns[0]].count()
-print("number of rows: " , count_rows)
+#print("number of rows: " , count_rows)
 
-#convert to boolean-value sothat to count the links
+#create an integer vector e and e_transpose has length(count_rows) with 1s
+e = np.ones(count_rows, dtype=int)
+e_T = np.array([e]).T
+
+#convert to boolean-value so that to count the links
 list = pd.isnull(df['Parent'])
-#print(list)
 
 count = 0
 arr_count = []
@@ -25,8 +31,10 @@ for i in list:
         count = 0
 
 arr_count.append(count)
+
+#delete the first value because the main link included
 arr_count = arr_count[1:]
-print(arr_count)
+#print(arr_count)
 
 matrix = np.zeros([count_rows, count_rows])
 #print(matrix)
@@ -41,18 +49,37 @@ df['Parent'] = df['Parent'].astype(np.int64)
 df['Child'] = df['Child'].fillna(0)
 df['Child'] = df['Child'].astype(np.int64)
 
-zeile_matrix = 0
+spalte_matrix = 0
 
-#loop the column to read the velue
+#loop the column to read the value not 0
 for i,row in df.iterrows():
     if df['Parent'][i] != 0:
         # - 1 for the index of matrix begins with 0
-        spalte_matrix = df['Parent'][i] - 1
+        zeile_matrix = df['Parent'][i] - 1
     else:
-        zeile_matrix = df['Child'][i] - 1
-        matrix[zeile_matrix][spalte_matrix] = 1 / arr_count[spalte_matrix]
+        spalte_matrix = df['Child'][i] - 1
+        matrix[zeile_matrix][spalte_matrix] = 1 / arr_count[zeile_matrix]
 
-print(matrix)
-np.savetxt('matrix.txt', matrix, fmt = '%.2f')
+#function for vector a is a dangling node and 0 otherwise
+def dangling_node_vector(matrix, count_rows):
+    y = np.zeros(count_rows, dtype=int)
+    for i in range(count_rows):
+        if sum(matrix[i]) == 0:
+            y[i] = 1
+    return y
 
+#function replaces the O_T rows with 1/n * e_T
+def stochastic_matrix(matrix, count_rows):
 
+    for i in range(count_rows):
+        if sum(matrix[i]) == 0:
+            matrix[i] = 1 / count_rows
+    return matrix
+
+vec_a = dangling_node_vector(matrix, count_rows)
+
+stochastic_matrix(matrix, count_rows)
+
+g_matrix = alpha * matrix + (1 - alpha) * (1 / count_rows) * e * e_T
+print(g_matrix)
+np.savetxt('matrix_test.txt', g_matrix, fmt = '%.5f')
